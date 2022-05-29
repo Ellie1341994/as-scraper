2eab9fceaf023f929f39db4fe877547f8ad040f9
ellie <eli1341994@gmail.com>
2022-05-29 19:31:38 -0300

    Change seat to row for proper semantic and decouple initial page vist to its own test

--- cypress/integration/seat_selection_spec.js
+++ cypress/integration/seat_selection_spec.js
@@ -4,21 +4,21 @@ const SECOND_SEAT_NUMBER = Cypress.env('second_seat') || 21;
 const PAGE = Cypress.env('page')
 describe('Travel seat selection with screenshot test', () => {
   let exitRowsWithItsCriteriaAccepted = []
-  let generateSeatSelectionFunction = (seat, visitPage = false) => () => {
-    visitPage && cy.visit(PAGE)
-    cy.get(`[aria-label*="Seat: ${seat}"]`)
-      .then(seats =>
-        {
+  let generateSeatSelectionFunction = (row) => () => {
+    cy.get(`[aria-label*="Seat: ${row}"]`)
+      .then(seats => {
           const RANDOM_INDEX = Math.floor(Math.random() * seats.length);
           seats[RANDOM_INDEX].click()
-          if(EXIT_ROWS.includes(seat) && !exitRowsWithItsCriteriaAccepted.includes(seat)){
+          if(EXIT_ROWS.includes(row) && !exitRowsWithItsCriteriaAccepted.includes(row)){
             cy.contains('Yes, I accept').click()
-            exitRowsWithItsCriteriaAccepted.push(seat)
+            exitRowsWithItsCriteriaAccepted.push(row)
           }
           cy.contains('Select').click()
-        }
-      )
+        })
   }
+  it('Visits seat selection test page',() => {
+    cy.visit(PAGE)
+  })
   it('Selects the first seat', generateSeatSelectionFunction(FIRST_SEAT_NUMBER, true))
   it('Moves to the next seat selection', () => {
     cy.contains("Next flight").click()
@@ -26,10 +26,9 @@ describe('Travel seat selection with screenshot test', () => {
   it('Selects the second seat', generateSeatSelectionFunction(SECOND_SEAT_NUMBER))
   it('Confirms selections and takes a screenshot', () => {
     cy.contains("Confirm Selection").click()
-      .then( () => {
-            cy.wait(3000)
-            cy.get(`section`).last().screenshot('Seat Selection')
-      }
-    )
+      .then(() => {
+        cy.wait(3000)
+        cy.get(`section`).last().screenshot('Seat Selection')
+      })
   })
 })
